6a8e3320597c4579fc38612a6eb4668ebe7e75d5
ptonini <pedro.tonini@hotmail.com>
2023-08-08 00:14:58 -0300

    fix: normalized end character of value strings

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "helm_release" "this" {
-  name = var.name
-  namespace = var.namespace
-  repository = var.repository
-  chart = var.chart
-  version = var.chart_version
-  values = var.values
-  max_history = var.max_history
+  name             = var.name
+  namespace        = var.namespace
+  repository       = var.repository
+  chart            = var.chart
+  version          = var.chart_version
+  values           = [for v in var.values : chomp(replace(v, "\r\n", "\n"))]
+  max_history      = var.max_history
   create_namespace = var.create_namespace
-  cleanup_on_fail = var.cleanup_on_fail
-  timeout = var.timeout
-  skip_crds = var.skip_crds
-  wait = var.wait
-  force_update = var.force_update
+  cleanup_on_fail  = var.cleanup_on_fail
+  timeout          = var.timeout
+  skip_crds        = var.skip_crds
+  wait             = var.wait
+  force_update     = var.force_update
 }
